ea957f9250d439660efd20785563da01eb8dd8d7
kartikekhurana <kartikekhurana@gmail.com>
2025-07-25 14:07:48 +0530

    changed the base URL

--- frontend/utils/axiosInstance.js
+++ frontend/utils/axiosInstance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://assignment-xv8h.onrender.com",
+  withCredentials: true,
 });
 
 export default axiosInstance;
